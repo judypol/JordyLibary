0fa7a496718a63771236d4ace73c5cab3d195d7b
judypol <lizhihua0320@126.com>
2015-01-28 14:40:41 +0800

    Add ORM folder
    Add ORM folder

---
+++ JordyLibary/ORM/Mappings/ProcParameterAttribute.cs
@@ -0,0 +1,48 @@
+﻿using JordyLibary.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace JordyLibary.ORM.Mappings
+{
+    /// <summary>
+    /// 存储过程参数描述
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property)]
+    public class ProcParameterAttribute : Attribute
+    {
+        /// <summary>
+        /// 参数名
+        /// </summary>
+        public string Name
+        {
+            get;
+            set;
+        }
+        /// <summary>
+        /// 参数类型
+        /// </summary>
+        public System.Data.ParameterDirection Direction
+        {
+            get;
+            set;
+        }
+        /// <summary>
+        /// 是否输出参数
+        /// </summary>
+        public bool Output
+        {
+            get;
+            set;
+        }
+        /// <summary>
+        /// 属性操作句柄
+        /// </summary>
+        public PropertyHandler Handler
+        {
+            get;
+            set;
+        }
+    }
+}
